5e36e080d4f8f13eede610c930743ee59032d482
jakelee <freejak5520@gmail.com>
2024-04-14 22:10:34 +0900

    임시 게시글 목록 페이지

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,30 @@
-const PostsPage = () => {
-  return <div>PostsPage</div>;
+"use server";
+
+import apiAxios from "@/lib/apiAxios";
+import PostItem from "./_components/PostItem";
+import Paginate from "./_components/Paginate";
+
+const Posts = async ({
+  searchParams: { page },
+}: {
+  searchParams: { page: string };
+}) => {
+  const {
+    data: { data, meta },
+  } = await apiAxios.get("/posts", {
+    params: {
+      page,
+    },
+  });
+
+  return (
+    <div>
+      {data.map((post: Post) => (
+        <PostItem key={post.id} post={post} />
+      ))}
+      <Paginate meta={meta} url="/posts" />
+    </div>
+  );
 };
 
-export default PostsPage;
+export default Posts;
