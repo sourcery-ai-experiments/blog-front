4aea9c02bd415ccfe4703a3305e907ce0ccdd386
Jake Lee <freejak5520@gmail.com>
2024-05-02 18:19:48 +0900

    lib 디렉토리 내 불필요한 API 관련 코드 제거
    - getFetch(API 요청), token(로그인 처리) 코드 제거
    - 게시글 slug 생성 함수에 docs 추가
    - constants.ts -> validation.ts 이름 변경(유효성 검사 관련된 상수만 포함하고 있기 때문에 변경)

--- src/lib/post.ts
+++ src/lib/post.ts
@@ -1,13 +1,18 @@
 import db from "@/lib/db";
 
+/**
+ * DB를 조회하며 slug 문자열이 사용가능한지 확인합니다.
+ *
+ * 사용 불가능한 slug일 경우 <original_slug>_1 처럼 sequence를 1씩 증가시키고
+ * `_<sequence>`를 suffix로 추가하여 DB에 존재하는지 반복 확인합니다.
+ *
+ * 사용 가능한 slug를 찾을 경우 반환합니다.
+ */
 const getAvailableSlug = async (
   originalSlug: string,
-  numberDuplicates: number = 0
-) => {
-  const nowSlug =
-    originalSlug + (numberDuplicates ? `_${numberDuplicates}` : "");
-
-  console.log();
+  sequence: number = 0
+): Promise<string> => {
+  const nowSlug = originalSlug + (sequence ? `_${sequence}` : "");
 
   const post = await db.post.findUnique({
     select: { id: true, slug: true },
@@ -15,12 +20,17 @@ const getAvailableSlug = async (
   });
 
   if (post) {
-    return await getAvailableSlug(originalSlug, numberDuplicates + 1);
+    return getAvailableSlug(originalSlug, sequence + 1);
   } else {
     return nowSlug;
   }
 };
 
+/**
+ * 게시글 제목(title)을 slug로 변환합니다.
+ *
+ * 중복된 slug일 경우 suffix를 추가해서 중복되지 않는 slug를 반환합니다.
+ */
 export async function makePostSlug(title: string) {
-  return await getAvailableSlug(encodeURI(title));
+  return getAvailableSlug(encodeURI(title));
 }
