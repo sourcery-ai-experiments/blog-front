da168a8335a5700d5fb1d72a943de58c1cf8150c
Jake Lee <freejak5520@gmail.com>
2024-05-04 01:58:18 +0900

    shadcn/ui 적용 및 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,29 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
 import Providers from "@/providers";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "블로그",
   description: "블로그",
 };
 
-const RootLayout = ({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) => {
+}>) {
   return (
     <Providers>
       <html lang="ko">
-        <body className={inter.className}>{children}</body>
+        <body className={cn("font-sans", fontSans.variable)}>{children}</body>
       </html>
     </Providers>
   );
-};
-
-export default RootLayout;
+}
