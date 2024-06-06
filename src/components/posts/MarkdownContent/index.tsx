87a9409c6b3bfedfdea416cd2127b02c3811e6da
Jake Lee <freejak5520@gmail.com>
2024-06-06 19:32:46 +0900

    상세페이지 스타일 적용

--- src/components/posts/MarkdownContent/index.tsx
+++ src/components/posts/MarkdownContent/index.tsx
@@ -2,20 +2,20 @@ import Markdown, { type Components } from "react-markdown";
 
 const markdownComponents = {
   h1: (props) => {
-    return <h1 className="mb-4 border-b text-3xl font-bold" {...props} />;
+    return <h1 className="mt-12 border-b text-4xl font-bold" {...props} />;
   },
   h2: (props) => {
-    return <h2 className="my-8 border-b pb-2 text-2xl font-bold" {...props} />;
+    return <h2 className="mt-10 border-b pb-2 text-3xl font-bold" {...props} />;
   },
   h3: (props) => {
-    return <h3 className="my-8 text-xl font-bold" {...props} />;
+    return <h3 className="mt-8 text-2xl font-bold" {...props} />;
   },
   img: (props) => {
     // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
-    return <img className="mx-auto my-12" {...props} />;
+    return <img className="mx-auto mt-6" {...props} />;
   },
   p: (props) => {
-    return <p className="my-6" {...props} />;
+    return <p className="mt-6" {...props} />;
   },
   a: (props) => {
     return <a className="text-blue-500 hover:underline" {...props} />;
