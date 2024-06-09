b17ce335d365bbd7fa7cd1267c2c2fbe290b5957
Jake Lee <freejak5520@gmail.com>
2024-06-09 11:21:13 +0900

    로그아웃 기능 수정, 시맨틱 태그 적용
    - 로그아웃 후 gnb가 갱신되지 않는 문제
    - div -> nav

--- src/components/gnb/index.tsx
+++ src/components/gnb/index.tsx
@@ -1,22 +1,25 @@
+"use server";
+
 import Link from "next/link";
-import { getSession } from "@/lib/session";
+import { getSession, logout } from "@/lib/session";
 
 export default async function Gnb() {
   const { id, email } = await getSession();
+
   return (
-    <div className="flex justify-between p-6">
+    <nav className="flex justify-between p-6">
       <Link href={"/"}>Home</Link>
 
       {id ? (
-        <div className="flex gap-4">
+        <form className="flex gap-4" action={logout}>
           <div>{email}</div>
-          <Link href={"/logout"}>Logout</Link>
-        </div>
+          <button type="submit">Logout</button>
+        </form>
       ) : (
         <div>
           <Link href={"/login"}>Log in</Link>
         </div>
       )}
-    </div>
+    </nav>
   );
 }
--- src/lib/session.ts
+++ src/lib/session.ts
@@ -4,6 +4,7 @@ import "server-only";
 
 import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 type SessionData = {
   id: number;
@@ -16,4 +17,12 @@ export async function getSession() {
     cookieName: process.env.NEXT_PUBLIC_TOKEN_COOKIE_NAME,
     password: process.env.AUTH_SECRET,
   });
+}
+
+export async function logout() {
+  const { destroy } = await getSession();
+
+  destroy();
+
+  redirect("/");
 }
