2df6cca523fb5b9793bd189a09f8d7e97ea3051f
Jake Lee <freejak5520@gmail.com>
2024-06-11 22:47:29 +0900

    로그인 페이지 디자인 수정
    라벨 추가
    폼 너비 변경

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,21 +1,29 @@
 "use client";
 
-import type { InputHTMLAttributes } from "react";
+import { useMemo, type InputHTMLAttributes } from "react";
 import { useFormStatus } from "react-dom";
 
 const Input = ({
   errors,
   className,
+  label,
   ...rest
 }: InputHTMLAttributes<HTMLInputElement> & {
   errors?: string[];
   className?: string;
+  label?: string;
 }) => {
   const { pending } = useFormStatus();
 
+  const id = useMemo(() => {
+    return rest.id ?? `${rest.name}_input`;
+  }, [rest.id, rest.name]);
+
   return (
     <div className="flex flex-col gap-2">
+      {label && <label htmlFor={id}>{label}</label>}
       <input
+        id={id}
         className={
           "w-full rounded border-none px-4 py-3.5 text-sm leading-5 ring-1 ring-inset " +
           className
